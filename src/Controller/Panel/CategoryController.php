c770cff1da064945b307dbfe2e42582e4324bdc3
Paweł Balicki <pawel@kilab.dev>
2023-12-15 12:17:48 +0100

    Add cache deletion in CategoryController and improve data fixtures generation

--- src/Controller/Panel/CategoryController.php
+++ src/Controller/Panel/CategoryController.php
@@ -7,16 +7,19 @@ namespace App\Controller\Panel;
 use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\ArticleRepository;
+use App\Repository\Cached\CacheKeyPrefix;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class CategoryController extends AbstractController
 {
     public function __construct(
         private ArticleRepository $articleRepository,
+        private CacheInterface $cache,
         private CategoryRepository $categoryRepository,
         private EntityManagerInterface $entityManager,
     ) {
@@ -156,6 +159,9 @@ class CategoryController extends AbstractController
         }
 
         $this->categoryRepository->moveArticlesFromCategoryTo($fromCategory, $toCategory);
+        $this->cache->delete(CacheKeyPrefix::ARTICLE_LATEST_FROM_CATEGORY);
+        $this->cache->delete(CacheKeyPrefix::CATEGORY_ALL);
+        $this->cache->delete(CacheKeyPrefix::CATEGORY_TOP);
 
         $this->addFlash('success', 'Artykuły zostały przeniesione');
 
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,20 +6,24 @@ use App\Entity\Article;
 use App\Entity\Category;
 use App\Entity\User;
 use App\Enum\ArticleStatus;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
-use Monolog\DateTimeImmutable;
 use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 
 class AppFixtures extends Fixture
 {
     public function __construct(
+        private EntityManagerInterface $entityManager,
         private PasswordHasherFactoryInterface $passwordHasherFactory,
     ) {
     }
 
     public function load(ObjectManager $manager): void
     {
+        $this->entityManager->getConfiguration()->getEntityListenerResolver()->clear();
+
         $faker = \Faker\Factory::create('pl_PL');
         $categories = [
             'Kategoria główna' => 'info',
@@ -43,7 +47,7 @@ class AppFixtures extends Fixture
         ];
 
         $admin = new User();
-        $admin->setCreatedAt(new DateTimeImmutable(false));
+        $admin->setCreatedAt(new DateTimeImmutable());
         $admin->setEmail('admin@g24.pl');
         $admin->setFullName('Andrzej Waliwąs');
         $admin->setImagePath('https://thispersondoesnotexist.com');
@@ -52,8 +56,13 @@ class AppFixtures extends Fixture
         $admin->setRoles(['ROLE_ADMIN']);
         $manager->persist($admin);
 
+        $articlesPerDay = 3;
+
         foreach ($categories as $name => $slug) {
             $articlesNumber = rand(175, 300);
+            $startDate = (new DateTimeImmutable())->modify('-' . intdiv($articlesNumber, $articlesPerDay) . ' days');
+            $articleCounter = 0;
+
             $category = new Category();
             $category->setColor($categoryColors[array_rand($categoryColors)]);
             $category->setIsRoot($slug === 'info');
@@ -67,13 +76,15 @@ class AppFixtures extends Fixture
             }
 
             for ($i = 0; $i < $articlesNumber; $i++) {
+                $date = $startDate->modify('+' . intdiv($articleCounter, $articlesPerDay) . ' days');
+                $date = $date->setTime(rand(8, 19), rand(0, 59));
+
                 $imgNumber = rand(1, 65);
-                $date = DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-2 years', 'now'));
                 $article = new Article();
                 $article->setAuthor($admin);
                 $article->setCategory($category);
-                $article->setCommentsNumber(rand(0, 23));
-                $article->setContent($this->replaceWithParagraphTags($faker->paragraphs(rand(3, 14), true)));
+                $article->setCommentsNumber(0);
+                $article->setContent($this->replaceWithParagraphTags($faker->paragraphs(rand(3, 15), true)));
                 $article->setCreatedAt($date);
                 $article->setExcerpt($faker->sentences(3, true));
                 $article->setImageUrl(rand(0, 3) !== 1 ? "/media/upload/article/2023/12/img$imgNumber.jpg" : null);
@@ -83,6 +94,7 @@ class AppFixtures extends Fixture
                 $article->setUpdateAuthor($admin);
 
                 $manager->persist($article);
+                $articleCounter++;
             }
 
             $manager->flush();
